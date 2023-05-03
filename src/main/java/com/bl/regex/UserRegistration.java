0b99513a7ac59a004494b621f3d9e1c39b545747
Sudarshan Gaikwad <sudarshangaikwad@gmailcom>
2023-05-03 21:38:32 +0530

    code added to UC-1 branch

--- src/main/java/com/bl/regex/UserRegistration.java
+++ src/main/java/com/bl/regex/UserRegistration.java
@@ -5,7 +5,9 @@ public class UserRegistration
     public static void main(String[] args) {
         System.out.println("Welcome To User Registration Programme");
 
+        ValidateUserInfo user = new ValidateUserInfo();
 
+        user.checkFirstName();
 
     }
 }
---
+++ src/main/java/com/bl/regex/ValidateUserInfo.java
@@ -0,0 +1,21 @@
+package com.bl.regex;
+import java.util.Scanner;
+import java.util.regex.*;
+public class ValidateUserInfo
+{
+    Scanner sc = new Scanner(System.in);
+
+    public void checkFirstName(){
+        System.out.print("Enter The First Name: ");
+        String firstName = sc.nextLine();
+
+
+
+        if(Pattern.matches("^[A-Z]{1}+[a-z]{3,}", firstName)){
+            System.out.println("First Name is Valid");
+        }else {
+            System.out.println("Invalid FirstName");
+        }
+    }
+
+}
